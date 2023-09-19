c3283357a3cbb752cba9ea3ba5fc801e7503aa8a
Travis Thurber <thurber@users.noreply.github.com>
2023-09-19 07:34:38 -0700

    update citations

--- src/routes/publications.svelte
+++ src/routes/publications.svelte
@@ -1,5 +1,6 @@
 <script>
     import {
+        Link,
         ListItem,
         UnorderedList,
         Tile,
@@ -13,12 +14,19 @@
     <UnorderedList class="ol" expressive>
         <ListItem class="ul-li li">
             <p>
-                Jones, A.D., Rastogi D., Vahmani P., Stansfield A., Reed K., Thurber T., Ullrich P., Rice J.S., Continental United States climate projections based on thermodynamic modification of historical weather, submitted.
+                Jones, A. D., D. Rastogi, P. Vahmani, A. Stansfield, K. Reed, T. Thurber, P.A. Ullrich, J. Rice (2023), Continental United States climate projections based on thermodynamic modification of historical weather, accepted at Nature Scientific Data, 
+                <Link target="_blank" href="https://doi.org/10.1038/s41597-023-02485-5">https://doi.org/10.1038/s41597-023-02485-5</Link>.
             </p>
         </ListItem>
         <ListItem class="ul-li li">
             <p>
                 Rastogi, D., J. Trok, N. Depsky, A.D. Jones, Evaluation of the characteristics of heat wave, drought, and their concurrence in the historical period and projected future changes in the CMIP6 GCMs, in prep.
+            </p>
+        </ListItem>
+        <ListItem class="ul-li li">
+            <p>
+                Srivastava, A. K., Ullrich, P. A., Rastogi, D., Vahmani, P., Jones, A., and Grotjahn, R.: Assessment of WRF (v 4.2.1) dynamically downscaled precipitation on subdaily and daily timescales over CONUS, Geosci. Model Dev., 16, 3699–3722, 
+                <Link target="_blank" href="https://doi.org/10.5194/gmd-16-3699-2023">https://doi.org/10.5194/gmd-16-3699-2023</Link>, 2023.
             </p>
         </ListItem>
     </UnorderedList>
